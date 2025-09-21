6bd6c54b950ba51621c88acb378cd8532899b8db
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-09-21 10:32:42 +0800

    added comment

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,38 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Image from "next/image";
 import * as React from 'react';
 
-// Augment the palette to include an ochre color
+// 使调色板包含自定义颜色
 declare module '@mui/material/styles' {
   interface Palette {
     ochre: Palette['primary'];
   }
-
   interface PaletteOptions {
     ochre?: PaletteOptions['primary'];
   }
 }
 
-// Update the Button's color options to include an ochre option
+// 更新按钮的颜色选项来包含自定义颜色
 declare module '@mui/material/Button' {
   interface ButtonPropsColorOverrides {
     ochre: true;
   }
 }
+
+let theme = createTheme({
+  // Theme customization goes here as usual, including tonalOffset and/or
+  // contrastThreshold as the augmentColor() function relies on these
+});
+
+const theme = createTheme(theme,{
+  palette: {
+    ochre: theme.palette.augmentColor({
+      color: {
+        main: '#E3D026',
+      },
+      name: 'ochre',
+    }),
+  },
+});
 
 export default function Home() {
   return (
